28a789c556135e1d4b32530d0b24e2f02b4f2c69
DylanDrein <dylandrein@gmail.com>
2017-02-17 00:21:11 +0000

    Histogram of mouseclicks across semester binned by click times (each day is a bin) partially working?

---
+++ SemesterTimeHist.py
@@ -0,0 +1,42 @@
+import json
+import csv
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Earliest UNIX time = 1412364244763 , Fri, 03 Oct 2014 19:24:04.763 GMT
+
+'''
+Get earliest date from results of SemesterTimeWrite script
+Find the date and then get midnight from that day
+create hist by adding multiples of 86400 onto that unix time
+'''
+
+#handle = open("./MouseDownEvents/MouseClicktimes.csv", 'rb')
+
+filepath = "./MouseDownEvents/MouseClicktimes.csv"
+
+times = []
+
+
+firstMidnight = 1412290800000
+daySeconds = 86400
+
+with open(filepath) as infile:
+			
+	parsed=csv.reader(infile)
+	for row in parsed:
+		times = np.array(list(row)).astype(np.float)
+
+bins = np.arange(firstMidnight, max(times), 86400)
+
+print max(bins) - min(bins) % daySeconds
+
+'''
+plt.hist(times, bins, facecolor='green')
+plt.title("$Histogram$ $of$ $Optimal/Actual$ $path$ $lengths$")
+plt.xlabel("$Value$")
+plt.ylabel("$Frequency$")
+plt.show()
+plt.clf()
+'''
---
+++ SemesterTimeWrite.py
@@ -0,0 +1,45 @@
+'''
+CSV FORMAT
+
+Anthony Coleman	1.42021E+12	137	375	mouseDown
+[      0             1       2   3     4]
+      Name			time 	 y   x    type
+'''
+
+
+import json
+import csv
+import os
+import numpy as np
+
+
+handle = open("./MouseDownEvents/MouseClicktimes.csv", 'w')
+
+filenum = 0
+
+times = []
+
+rootdir = "./CSVSeparated/"
+
+parsed = ""
+
+for current_directory, directories, files in os.walk(rootdir):
+	
+	for file in files:
+		filenum += 1
+		filepath = os.path.join(current_directory,file)
+		
+		with open(filepath) as infile:
+			
+			parsed=csv.reader(infile)
+			for row in parsed:
+
+				if(row[4] == "mouseDown"):
+					times.append(float(row[1]))
+					#print type(row[1])
+
+		print filenum
+
+c = csv.writer(handle, delimiter = ',')
+times.sort()
+c.writerow(list(times))
