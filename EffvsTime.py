cea0df6bbefa76bc8b4e811b270f54bd2369bee0
DylanDrein <dylandrein@gmail.com>
2017-02-23 03:07:06 +0000

    Working

--- EffvsTime.py
+++ EffvsTime.py
@@ -36,6 +36,11 @@ optimalVals2 = []
 times1 = []
 times2 = []
 
+totaltimes1 = []
+totaltimes2 = []
+totalefficiency1 = []
+totalefficiency2 = []
+
 hourmilliseconds = 3600000
 
 firstlabmidnight = 1415318400000 # 7th of november
@@ -56,16 +61,17 @@ for current_directory, directories, files in os.walk(rootdir):
 	for file in files:
 		filenum += 1
 		filepath = os.path.join(current_directory,file)
-		
+		measuring = False
+
 		with open(filepath) as infile:
 			parsed = csv.reader(infile)
+
 			for row in parsed:
 
 				filename = row[0].lower().replace(" ", "-")
 
-
 				# LAB ONE
-				if(float(row[1]) > lab1start and float(row[1]) < lab1end):
+				if((float(row[1]) > lab1start) and (float(row[1]) < lab1end)):
 					if(row[4] == "mouseDown"):
 						measuring = True
 						startPoint = np.array((float(row[3]), float(row[2])))
@@ -73,7 +79,6 @@ for current_directory, directories, files in os.walk(rootdir):
 						startTime = float(row[1])
 						prevTime = float(row[1])
 						
-
 					if((float(row[1]) < (startTime - 1500) or row[4] == "mouseUp") and measuring):
 						measuring = False
 						optimalDist = dist(prevPoint, startPoint)
@@ -94,6 +99,7 @@ for current_directory, directories, files in os.walk(rootdir):
 						#print "Optimal Distance: " + str(optimalDist) + "\n"
 						optimalDist = 0
 						actualDist = 0
+						startTime = 0
 
 					if(measuring == True):
 						currentPos = np.array((float(row[3]), float(row[2])))
@@ -105,8 +111,11 @@ for current_directory, directories, files in os.walk(rootdir):
 					prevPoint = np.array((float(row[3]), float(row[2])))
 					prevTime = float(row[1])
 
+				if(float(row[1]) > lab1end and float(row[1]) < lab2start):
+					measuring = False
+
 				# LAB TWO
-				if(float(row[1]) > lab2start and float(row[1]) < lab2end):
+				if((float(row[1]) > lab2start) and (float(row[1]) < lab2end)):
 					if(row[4] == "mouseDown"):
 						measuring = True
 						startPoint = np.array((float(row[3]), float(row[2])))
@@ -136,6 +145,7 @@ for current_directory, directories, files in os.walk(rootdir):
 						#print "Optimal Distance: " + str(optimalDist) + "\n"
 						optimalDist = 0
 						actualDist = 0
+						startTime = 0
 
 					if(measuring == True):
 						currentPos = np.array((float(row[3]), float(row[2])))
@@ -146,9 +156,10 @@ for current_directory, directories, files in os.walk(rootdir):
 
 					prevPoint = np.array((float(row[3]), float(row[2])))
 					prevTime = float(row[1])
-				
-			if(float(row[1]) > lab1start and float(row[1]) < lab1end):
-				if(measuring == True):
+			
+			
+			if(measuring == True):	
+				if((float(row[1]) > lab1start) and (float(row[1]) < lab1end)):
 					optimalDist = dist(prevPoint, startPoint)
 
 					if(actualDist != 0):
@@ -162,12 +173,11 @@ for current_directory, directories, files in os.walk(rootdir):
 
 					optimalDist = 0
 					actualDist = 0
+					startTime = 0
 
-			if(float(row[1]) > lab2start and float(row[1]) < lab2end):
-				if(measuring == True):
+				if((float(row[1]) > lab2start) and (float(row[1]) < lab2end)):
 					optimalDist = dist(prevPoint, startPoint)
 					
-
 					if(actualDist != 0):
 						efficiency2.append(optimalDist/actualDist)
 
@@ -179,57 +189,37 @@ for current_directory, directories, files in os.walk(rootdir):
 
 					optimalDist = 0
 					actualDist = 0
-					'''
-					print "Actual distance: " + str(actualDist)
-					print "Average actual speed: " + str(np.mean(velocities)) + "px/ms"
-					print "Time taken: " + str(timeTaken) + "ms"
-					print "Optimal Distance: " + str(optimalDist) + "\n"
-					print filenum
-					print actualnum
-					print optimalnum
-					'''			
+					startTime = 0
+				measuring = False
+
+		totalefficiency1.append(efficiency1)
+		totalefficiency2.append(efficiency2)
+		totaltimes1.append(times1)
+		totaltimes2.append(times2)
 		
+
+
+		'''
 		times1 = np.array(times1)
 		times2 = np.array(times2)
 
-		
-
-		'''
-		#DOT PLOT (WORKS!!)
-		maxVals = [max(optimalVals), max(actualVals)]
-		plt.scatter(actualVals, optimalVals, c=times, cmap=plt.cm.winter)
-		x = max(maxVals)
-		plt.plot([0, x], [0, x], 'k-')
-		plt.title("$Graph$ $of$ $Efficiency$ $(Optimal/Actual)$")
-		plt.ylabel("$Optimal$ $Mouse$ $Path$ $Lengths$ $(px)$")
-		plt.xlabel("$Actual$ $Mouse$ $Path$ $Lengths$ $(px)$")
-		plt.axis([0, max(actualVals) + 100, 0, max(optimalVals) + 100])
-		plt.savefig('./ScatterPlots2/' + filename + '.png')
-		plt.clf()
-		
-
-		print len(times1)
-		print len(times2)
-		print len(efficiency1)
-		print len(efficiency2)
-		'''
-
+		m1, b1 = np.polyfit(times1, efficiency1, 1)
+		m2, b2 = np.polyfit(times2, efficiency2, 1)
 	
 		f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 		#.title("Graph of efficiency over time")
-		ax1.scatter(times1, efficiency1)
-		ax2.scatter(times2, efficiency2)
 		ax1.set_title("Lab 1: 11:00-13:00 7/11/14")
+		ax1.scatter(times1, efficiency1)
+		ax1.plot(times1, m1*times1 + b1, '-')
 		ax2.set_title("Lab 2: 11:00-13:00 28/11/14")
+		ax2.scatter(times2, efficiency2)
+		ax2.plot(times2, m2*times2 + b2, '-')
+		plt.xticks(rotation = 'vertical')
+		plt.subplots_adjust(bottom=0.2)
 		plt.savefig('./ScatterPlots2/' + filename + '.png')
 		plt.clf()
+		'''
 		
-
-		print len(times1)
-		print len(times2)
-		print len(optimalVals1)
-		print len(optimalVals2)
-		print filenum
 
 		del actualVals1[:]
 		del optimalVals1[:]
@@ -242,3 +232,21 @@ for current_directory, directories, files in os.walk(rootdir):
 		times2 = []
 		del times1[:]
 		del times2[:]
+
+		print filenum
+
+m1, b1 = np.polyfit(totaltimes1, totalefficiency1, 1)
+m2, b2 = np.polyfit(totaltimes2, totalefficiency2, 1)
+
+f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
+#.title("Graph of efficiency over time")
+ax1.set_title("Lab 1: 11:00-13:00 7/11/14")
+ax1.scatter(totaltimes1, totalefficiency1)
+ax1.plot(totaltimes1, m1*totaltimes1 + b1, '-')
+ax2.set_title("Lab 2: 11:00-13:00 28/11/14")
+ax2.scatter(totaltimes2, totalefficiency2)
+ax2.plot(totaltimes2, m2*totaltimes2 + b2, '-')
+plt.xticks(rotation = 'vertical')
+plt.subplots_adjust(bottom=0.2)
+plt.savefig('./ScatterPlots2/' + "xtotallabs" + '.png')
+plt.clf()
