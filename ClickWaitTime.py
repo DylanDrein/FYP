6d8e2b4d51f8483fd7f5462866ea575bb467914c
DylanDrein <dylandrein@gmail.com>
2017-03-06 01:52:40 +0000

    Early attempt at script which measures hover time before user clicks

--- ClickWaitTime.py
+++ ClickWaitTime.py
@@ -2,11 +2,12 @@
 A script which measures the time a user rests the mouse on the target before clicking
 '''
 
-import os
-from operator import itemgetter, attrgetter, methodcaller
+import os, sys
 import json
+import numpy as np
+import csv
 
-rootdir ='./UsersReversed/clarence-roberts-reversed.json'
+rootdir = './CSVSeparated/'
 
 checkNum = 0
 
@@ -18,34 +19,43 @@ initialTime = 0
 clickTime = 0
 hoverTime = 0
 
-#for current_directory, directories, files in os.walk(rootdir):
+hovertimesinlab = []
+hovertimesoutlab = []
+
+'''
+CSV FORMAT
+
+Anthony Coleman	1.42021E+12	137	375	mouseDown
+[      0             1       2   3     4]
+      Name			time 	 y   x    type
+'''
+
+for current_directory, directories, files in os.walk(rootdir):
 	
-	#for file in files:
-		
-	#	filenum += 1
-	#	filepath = os.path.join(current_directory,file)
-
-		#with open(filepath) as infile:
-with open(rootdir) as infile:
-
-	for line in infile:
+	for file in files:
+		filenum += 1
+		filepath = os.path.join(current_directory,file)
 	
-		parsed = json.loads(line)
+
+		with open(filepath) as infile:
+			parsed = csv.reader(infile)
+
+			for row in parsed:
 		
-		name = parsed['name']
+				name = parsed[0]
 
-		if(checkNum == 1):
-			clickTime = parsed['time']
-			hoverTime = initialTime - clickTime
-			checkNum = 0
-		
-		if(parsed['type'] == "mouseDown"):
-			checkNum = 1
-			initialTime = parsed['time']
-			hoverTime = 0
-			clickTime = 0
+				if(checkNum == 1 and parsed[0] == initialTime):
+					clickTime = float(parsed[0])
+					hoverTime = initialTime - clickTime
+					checkNum = 0
+				
+				if(parsed[4] == "mouseDown"):
+					checkNum = 1
+					initialTime = float(parsed[1])
+					hoverTime = 0
+					clickTime = 0
 
-		if(hoverTime != 0):
-			print hoverTime
+				if(hoverTime != 0):
+					print hoverTime
 
-		hoverTime = 0
+				hoverTime = 0
