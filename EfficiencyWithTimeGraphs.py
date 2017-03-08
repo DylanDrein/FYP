Corrected labels for sample rather than population, added variance label

EfficiencyWithTimeGraphs.py
@@ -36,20 +36,24 @@ with handle4 as infile:
 		outlabtimes = np.array(list(row)).astype(np.float)
 
 
-#bins = np.arange(0, 1.01, 0.01)
-'''
+bins = np.arange(0, 1.01, 0.01)
+
 print len(inlabeff)
 print len(outlabeff)
 print len(inlabtimes)
 print len(outlabtimes)
 
-#inlabeff = np.log(inlabeff)
-#outlabeff = np.log(outlabeff)
+#inlabeff = np.log2(inlabeff)
+#outlabeff = np.log2(outlabeff)
+
+print inlabeff
+print outlabeff
+
 
 sdvinlab = np.std(inlabeff)
 meaninlab = np.mean(inlabeff)
 varinlab = np.var(inlabeff)
-label1 = ["$\mu$: " + str(meaninlab) + "\n" + "$\sigma$: " + str(sdvinlab)]
+label1 = ["$\overline{x}$ = " + str(meaninlab) + "\n" + "$s$ = " str(sdvinlab) + "\n" + "$s^2$ = " + str(varinlab)]
 plt.hist(inlabeff, bins, label = label1)
 #plt.hist(inlabeff, bins, label = label1)
 #plt.title("Probability density $f(x)$ of in-lab mousepath efficiency \n weighted by duration of mouse sequence")
@@ -59,15 +63,15 @@ plt.ylabel("Frequency (# mouse path sequences)")
 #plt.ylabel("Weighted probability density $f(x)$")
 #plt.xticks(np.arange(0, 1.1, 0.1), rotation = 'vertical')
 plt.legend(loc="best")
-#plt.show()
-plt.savefig('./FinalGraphs/InLabEfficiencyHist.png')
+plt.show()
+#plt.savefig('./FinalGraphs/InLabEfficiencyHist.png')
 #plt.savefig('./Metricgraphs/' + "WeightedinLabHist" + '.png')
 plt.clf()
 
 sdvoutlab = np.std(outlabeff)
 meanoutlab = np.mean(outlabeff)
 varoutlab = np.var(outlabeff)
-label2 = ["$\mu$: " + str(meanoutlab) + "\n" + "$\sigma$: " + str(sdvoutlab)]
+label1 = ["$\overline{x}$ = " + str(meanoutlab) + "\n" + "$s$ = " str(sdvoutlab) + "\n" + "$s^2$ = " + str(varoutlab)]
 plt.hist(outlabeff, bins, label = label2)
 #plt.hist(outlabeff, bins, label = label2)
 #plt.title("Probability density $f(x)$ of out-of-lab mousepath efficiency \n weighted by duration of mouse sequence")
@@ -76,8 +80,8 @@ plt.xlabel("Efficiency (Optimal mousepath length / Actual mousepath length)")
 plt.ylabel("Frequency (# mouse path sequences)")
 #plt.xticks(np.arange(0, 1.1, 0.1), rotation = 'vertical')
 plt.legend(loc="best")
-#plt.show()
-plt.savefig('./FinalGraphs/OutLabEfficiencyHist.png')
+plt.show()
+#plt.savefig('./FinalGraphs/OutLabEfficiencyHist.png')
 #plt.savefig('./Metricgraphs/' + "WeightedOutOfLabHist" + '.png')
 plt.clf()
 
@@ -88,11 +92,11 @@ print sdvoutlab
 print meanoutlab
 print varoutlab
 '''
-
 data = [inlabeff, outlabeff]
-plt.boxplot(data, 1, '', labels=["In lab", "Out of lab"])
+plt.boxplot(data, 0, '', labels=["In lab", "Out of lab"])
 plt.title("Efficiency boxplot")
 plt.ylabel('Efficiency (optimal/actual path lengths)')
-plt.savefig('./FinalGraphs/TotalEfficiencyBoxPlot.png')
-#plt.show()
+#plt.savefig('./FinalGraphs/TotalEfficiencyBoxPlotNoNotchesNoOutliers.png')
+plt.show()
 plt.clf()
+'''