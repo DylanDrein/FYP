95462ff14ca3b276e650390cbe48be98c048c1dc
DylanDrein <dylandrein@gmail.com>
2017-03-10 02:50:20 +0000

    should work

--- OvershootGraph.py
+++ OvershootGraph.py
@@ -37,7 +37,7 @@ with handle8 as infile:
 	for row in parsed:
 		outlabovershoot = np.array(list(row)).astype(np.float)
 
-bins = np.arange(0, 600, 10)
+bins = np.arange(0, 225, 5)
 
 #print len(inlabspeed)
 #print len(outlabspeed)
@@ -55,25 +55,25 @@ meanoutlab = np.mean(outlabovershoot)
 varoutlab = np.var(inlabovershoot)
 
 label1 = ["$\overline{x}$ = " + str(meaninlab) + "\n" + "$s$ = " + str(sdvinlab) + "\n" + "$s^2$ = " + str(varinlab)]
-plt.hist(inlabovershoot, label = label1)
+plt.hist(inlabovershoot, bins, label = label1)
 plt.title("Histogram of mouse path overshoot in lab environment")
 plt.xlabel("Overshoot (px)")
 plt.ylabel("Frequency (# of mouse path sequences)")
-#plt.xticks(np.arange(0, 1.1, 0.1), rotation = 'vertical')
+plt.xticks(np.arange(0, 225, 25), rotation = 'vertical')
 plt.legend(loc="best")
-plt.show()
-#plt.savefig('./Metricgraphs/' + "inLabPDFexcl" + '.png')
+#plt.show()
+plt.savefig('./finalgraphs/In_Lab_Histogram_Unweighted.png')
 plt.clf()
 
 label2 = ["$\overline{x}$ = " + str(meanoutlab) + "\n" + "$s$ = " + str(sdvoutlab) + "\n" + "$s^2$ = " + str(varoutlab)]
-plt.hist(outlabovershoot, label = label2)
+plt.hist(outlabovershoot, bins, label = label2)
 plt.title("Histogram of mouse path overshoot out of lab environment")
 plt.xlabel("Overshoot (px)")
 plt.ylabel("Frequency (# of mouse path sequences)")
-#plt.xticks(np.arange(0, 1.1, 0.1), rotation = 'vertical')
+plt.xticks(np.arange(0, 225, 25), rotation = 'vertical')
 plt.legend(loc="best")
-plt.show()
-#plt.savefig('./Metricgraphs/' + "OutOfLabPDFexcl" + '.png')
+#plt.show()
+plt.savefig('./finalgraphs/Out_Of_Lab_Histogram_Unweighted.png')
 plt.clf()
 
 print meaninlab
@@ -85,12 +85,11 @@ print varoutlab
 print max(inlabovershoot)
 print max(outlabovershoot)
 
-'''
+
 data = [inlabspeed, outlabspeed]
-plt.boxplot(data, 1)
-plt.title("Total in lab speediciency (left) vs. Total out of lab speediciency (right)")
+plt.boxplot(data, 0, '', labels=["In lab", "Out of lab"])
+plt.title("Total in lab mouse path overshoot")
 plt.ylabel('speediciency (optimal/actual path lengths')
-#plt.savefig('./Metricgraphs/' + "TotalBoxPlot" + '.png')
-plt.show()
+plt.savefig('./finalgraphs/Total_Overshoot_BoxPlot_No_Outliers_No_Notches.png')
+#plt.show()
 plt.clf()
-'''
