fd0fdc704198e8746fc7696749038a9c8c44d17a
DylanDrein <dylandrein@gmail.com>
2017-03-10 02:17:53 +0000

    Fixed boxplots

--- SpeedwithTime.py
+++ SpeedwithTime.py
@@ -64,28 +64,24 @@ label1 = ["$\overline{x}$ = " + str(meaninlab) + "\n" + "$s$ = " + str(sdvinlab)
 plt.hist(inlabspeed, bins, label = label1)
 #plt.hist(inlabspeed, bins, label = label1)
 plt.title("Histogram of mouse movement speed in lab environment")
-#plt.title("Histogram of in-lab mousepath efficiency")
 plt.xlabel("Speed $(px/s)$")
-#plt.ylabel("# of mouse path sequences")
 plt.ylabel("Frequency (# of mouse movements)")
 #plt.xticks(np.arange(0, 1.1, 0.1), rotation = 'vertical')
 plt.legend(loc="best")
-plt.show()
-#plt.savefig('./finalgraphs/' + "inLabPDFexcl" + '.png')
+#plt.show()
+plt.savefig('./finalgraphs/In_Lab_Speed_Histogram_Unweighted.png')
 plt.clf()
 
 label2 = ["$\overline{x}$ = " + str(meanoutlab) + "\n" + "$s$ = " + str(sdvoutlab) + "\n" + "$s^2$ = " + str(varoutlab)]
 plt.hist(outlabspeed, bins, label = label2)
 #plt.hist(outlabspeed, bins, label = label2)
 plt.title("Histogram of mouse movement speed out of lab environment")
-#plt.title("Histogram of out-of-lab mousepath efficiency")
 plt.xlabel("Speed $(px/s)$")
-#plt.ylabel("# of mouse path sequences")
 plt.ylabel("Frequency (# of mouse movements)")
 #plt.xticks(np.arange(0, 1.1, 0.1), rotation = 'vertical')
 plt.legend(loc="best")
-plt.show()
-#plt.savefig('./finalgraphs/' + "OutOfLabPDFexcl" + '.png')
+#plt.show()
+plt.savefig('./finalgraphs/Out_Of_Lab_Speed_Histogram_Unweighted.png')
 plt.clf()
 
 
@@ -99,12 +95,11 @@ print varoutlab
 print max(inlabspeed)
 print max(outlabspeed)
 
-'''
+
 data = [inlabspeed, outlabspeed]
-plt.boxplot(data, 1)
-plt.title("Total in lab efficiency (left) vs. Total out of lab efficiency (right)")
-plt.ylabel('Efficiency (optimal/actual path lengths')
+plt.boxplot(data, 0, '', labels=["In lab", "Out of lab"])
+plt.title("Speed of mouse sequence")
+plt.ylabel("Speed $(px/s)$")
 #plt.savefig('./finalgraphs/' + "TotalBoxPlot" + '.png')
 plt.show()
 plt.clf()
-'''
