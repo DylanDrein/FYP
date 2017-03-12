1a51919a8bc4a4b0f4c788a76f7e3f43dff2a004
DylanDrein <dylandrein@gmail.com>
2017-03-12 23:10:51 +0000

    Updating file paths

--- Out_Lab_Y_Axes_Graphs.py
+++ Out_Lab_Y_Axes_Graphs.py
@@ -7,12 +7,15 @@ import matplotlib.pyplot as plt
 handle1 = open("./metricstest/outlabyactual.csv", 'rb')
 handle2 = open("./metricstest/outlabyoptimal.csv", 'rb')
 
+outlabyact = []
+outlabyopt = []
+
 with handle1 as infile:
 	parsed=csv.reader(infile)
 	for row in parsed:
-		print len(row)
+		outlabyact.append(np.array(list(row)).astype(np.float))
 
 with handle2 as infile:
 	parsed=csv.reader(infile)
 	for row in parsed:
-		print len(row)
+		outlabyopt.append(np.array(list(row)).astype(np.float))
