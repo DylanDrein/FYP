0c1e1dedcfd951b0036ef2f7b1cb42e2b7c0e085
DylanDrein <dylandrein@gmail.com>
2017-03-12 23:10:25 +0000

    Updating file paths

--- In_Lab_Y_Axes_Graphs.py
+++ In_Lab_Y_Axes_Graphs.py
@@ -7,12 +7,15 @@ import matplotlib.pyplot as plt
 handle1 = open("./metricstest/inlabyactual.csv", 'rb')
 handle2 = open("./metricstest/inlabyoptimal.csv", 'rb')
 
+inlabyact = []
+inlabyopt = []
+
 with handle1 as infile:
 	parsed=csv.reader(infile)
 	for row in parsed:
-		print len(row)
+		inlabyact.append(np.array(list(row)).astype(np.float))
 
 with handle2 as infile:
 	parsed=csv.reader(infile)
 	for row in parsed:
-		print len(row)
+		inlabyopt.append(np.array(list(row)).astype(np.float))
