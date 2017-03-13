e7dc0fffa889cee113870bec0c4c1aaeb8f0bd8d
DylanDrein <dylandrein@gmail.com>
2017-03-13 16:09:48 +0000

    Individual variable changes for each file

--- Out_Lab_Y_Axes_Graphs.py
+++ Out_Lab_Y_Axes_Graphs.py
@@ -4,8 +4,10 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-handle1 = open("./metricstest/outlabyactual.csv", 'rb')
-handle2 = open("./metricstest/outlabyoptimal.csv", 'rb')
+handle1 = open("./metricstest/outlabyactual - copy.csv", 'rb')
+handle2 = open("./metricstest/outlabyoptimal - copy.csv", 'rb')
+
+#np.seterr(divide="raise")
 
 outlabyact = []
 outlabyopt = []
@@ -13,9 +15,32 @@ outlabyopt = []
 with handle1 as infile:
 	parsed=csv.reader(infile)
 	for row in parsed:
-		outlabyact.append(np.array(list(row)).astype(np.float))
+		outlabyact = np.array(list(row)).astype(np.float64)
 
 with handle2 as infile:
 	parsed=csv.reader(infile)
 	for row in parsed:
-		outlabyopt.append(np.array(list(row)).astype(np.float))
+		outlabyopt = np.array(list(row)).astype(np.float64)
+
+absolutesubtract = np.subtract(outlabxopt, outlabxact)
+absoluteerror = np.abs(absolutesubtract)
+#outlabxoptabs = np.abs(outlabxopt)
+
+#absoluteerror = np.true_divide(absoluteerror, outlabxoptabs)
+
+print absoluteerror
+
+#print absoluteerror
+
+sdvoutlab = np.std(absoluteerror)
+meanoutlab = np.mean(absoluteerror)
+varoutlab = np.var(absoluteerror)
+
+bins = np.arange(0, 250, 1)
+label1 = ["$\overline{x}$ = " + str(meanoutlab) + "\n" + "$s$ = " + str(sdvoutlab) + "\n" + "$s^2$ = " + str(varoutlab) + "\n" + "Max value: " + str(max(absoluteerror)) + "\n" + "Min value: " + str(min(absoluteerror))]
+plt.hist(absoluteerror, bins, label = label1)
+plt.title("Absolute error in x-axis movement in lab environment")
+plt.xlabel("Error (px)")
+plt.ylabel("Frequency (# of mouse path sequences)")
+plt.legend(loc="best")
+plt.show()
