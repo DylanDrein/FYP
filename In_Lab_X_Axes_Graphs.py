05127f22758c508f42092c357b4e44ab5ad43f0c
DylanDrein <dylandrein@gmail.com>
2017-03-13 15:06:25 +0000

    Abandoning trying to do relative error for the minute, stick to absolute error (better than nothing...) not ideal

--- In_Lab_X_Axes_Graphs.py
+++ In_Lab_X_Axes_Graphs.py
@@ -4,8 +4,10 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-handle1 = open("./metricstest/inlabxactual.csv", 'rb')
-handle2 = open("./metricstest/inlabxoptimal.csv", 'rb')
+handle1 = open("./metricstest/inlabxactual - copy.csv", 'rb')
+handle2 = open("./metricstest/inlabxoptimal - copy.csv", 'rb')
+
+#np.seterr(divide="raise")
 
 inlabxact = []
 inlabxopt = []
@@ -13,9 +15,34 @@ inlabxopt = []
 with handle1 as infile:
 	parsed=csv.reader(infile)
 	for row in parsed:
-		inlabxact.append(np.array(list(row)).astype(np.float))
+		inlabxact = np.array(list(row)).astype(np.float64)
 
 with handle2 as infile:
 	parsed=csv.reader(infile)
 	for row in parsed:
-		inlabxopt.append(np.array(list(row)).astype(np.float))
+		inlabxopt = np.array(list(row)).astype(np.float64)
+
+absolutesubtract = np.subtract(inlabxopt, inlabxact)
+absoluteerror = np.abs(absolutesubtract)
+#inlabxoptabs = np.abs(inlabxopt)
+
+#absoluteerror = np.true_divide(absoluteerror, inlabxoptabs)
+
+print absoluteerror
+
+#print absoluteerror
+
+sdvinlab = np.std(absoluteerror)
+meaninlab = np.mean(absoluteerror)
+varinlab = np.var(absoluteerror)
+
+
+
+bins = np.arange(0, 1000, 1)
+label1 = ["$\overline{x}$ = " + str(meaninlab) + "\n" + "$s$ = " + str(sdvinlab) + "\n" + "$s^2$ = " + str(varinlab)]
+plt.hist(absoluteerror, bins, label = label1)
+plt.title("absoluteerror")
+plt.xlabel("error")
+plt.ylabel("Frequency (# of mouse path sequences)")
+plt.legend(loc="best")
+plt.show()
