7cccd857ce99bbb59a1c3bcc5a43a086caa174f2
DylanDrein <dylandrein@gmail.com>
2017-03-18 00:37:55 +0000

    Overhauled, handles all t tests now

--- ttest.py
+++ ttest.py
@@ -9,18 +9,60 @@ from scipy import stats
 
 handleineff = open("./Metricstest/inlabeff.csv", 'rb')
 handleouteff = open("./Metricstest/outlabeff.csv", 'rb')
+
 handleintimes = open("./Metricstest/inlabtimes.csv", 'rb')
 handleouttimes = open("./Metricstest/outlabtimes.csv", 'rb')
 
-handle5 = open("./Metricstest/inlabspeed.csv", 'rb')
-handle6 = open("./Metricstest/outlabspeed.csv", 'rb')
+handleinspeed = open("./Metricstest/inlabspeed.csv", 'rb')
+handleoutspeed = open("./Metricstest/outlabspeed.csv", 'rb')
+
+#overshoot
+handleinovershoot = open("./Metricstest/inlabovershoot.csv", 'rb')
+handleoutovershoot = open("./Metricstest/outlabovershoot.csv", 'rb')
+
+#hovertime
+handleinhovertime = open("./Metricstest/inlabhovertime.csv", 'rb')
+handleouthovertime = open("./Metricstest/outlabhovertime.csv", 'rb')
+
+#x axis
+handleinxact = open("./metricstest/inlabxactual - copy.csv", 'rb')
+handleinxopt = open("./metricstest/inlabxoptimal - copy.csv", 'rb')
+handleoutxact = open("./metricstest/outlabxactual - copy.csv", 'rb')
+handleoutxopt = open("./metricstest/outlabxoptimal - copy.csv", 'rb')
+
+#y axis
+handleinyact = open("./metricstest/inlabyactual - copy.csv", 'rb')
+handleinyopt = open("./metricstest/inlabyoptimal - copy.csv", 'rb')
+handleoutyact = open("./metricstest/outlabyactual - copy.csv", 'rb')
+handleoutyopt = open("./metricstest/outlabyoptimal - copy.csv", 'rb')
+
 
 inlabeff = []
 outlabeff = []
+
 inlabtimes = []
 outlabtimes = []
+
 inlabspeeds = []
 outlabspeeds = []
+
+inovershoot = []
+outovershoot = []
+
+inhovertime = []
+outhovertime = []
+
+inlabxact = []
+inlabxopt = []
+
+outlabxact = []
+outlabxopt = []
+
+inlabyact = []
+inlabyopt = []
+
+outlabyact = []
+outlabyopt = []
 
 
 with handleineff as infile:
@@ -46,21 +88,126 @@ with handleouttimes as infile:
 		outlabtimes = np.array(list(row)).astype(np.float32)
 
 
-with handle5 as infile:
+with handleinspeed as infile:
 	parsed=csv.reader(infile)
 	for row in parsed:
 		inlabspeeds = np.array(list(row)).astype(np.float32)
 
-with handle6 as infile:
+with handleoutspeed as infile:
 	parsed=csv.reader(infile)
 	for row in parsed:
 		outlabspeeds = np.array(list(row)).astype(np.float32)
 
-#n = scipy.stats.ranksums(inlabeff, outlabeff)
-#n = scipy.stats.mannwhitneyu(outlabeff, inlabeff, alternative='greater')
-#print "t-statistic is %.10f and the p-value is %.60f." % n
+
+
+with handleinovershoot as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		inovershoot = np.array(list(row)).astype(np.float32)
+
+with handleoutovershoot as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		outovershoot = np.array(list(row)).astype(np.float32)
+
+with handleinhovertime as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		inhovertime = np.array(list(row)).astype(np.float32)
+
+with handleouthovertime as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		outhovertime = np.array(list(row)).astype(np.float32)
+
+#inhovertime = np.ma.masked_equal(inhovertime,0.0)
+#outhovertime = np.ma.masked_equal(outhovertime,0.0)
+
+with handleinxact as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		inlabxact = np.array(list(row)).astype(np.float64)
+
+with handleinxopt as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		inlabxopt = np.array(list(row)).astype(np.float64)
+
+with handleoutxact as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		outlabxact = np.array(list(row)).astype(np.float64)
+ 
+with handleoutxopt as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		outlabxopt = np.array(list(row)).astype(np.float64)
 
 '''
+inabsolutesubtract = np.subtract(inlabxopt, inlabxact)
+inabsoluteerror = np.abs(inabsolutesubtract)
+inlabxoptabs = np.abs(inlabxopt)
+inxrelativeerror = np.true_divide(inabsoluteerror, inlabxoptabs)
+
+outabsolutesubtract = np.subtract(outlabxopt, outlabxact)
+outabsoluteerror = np.abs(outabsolutesubtract)
+outlabxoptabs = np.abs(outlabxopt)
+outxrelativeerror = np.true_divide(outabsoluteerror, outlabxoptabs)
+
+
+with handleinyact as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		inlabyact = np.array(list(row)).astype(np.float64)
+
+with handleinyopt as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		inlabyopt = np.array(list(row)).astype(np.float64)
+
+with handleoutyact as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		outlabyact = np.array(list(row)).astype(np.float64)
+ 
+with handleoutyopt as infile:
+	parsed=csv.reader(infile)
+	for row in parsed:
+		outlabyopt = np.array(list(row)).astype(np.float64)
+
+inabsolutesubtract = np.subtract(inlabyopt, inlabyact)
+inabsoluteerror = np.abs(inabsolutesubtract)
+inlabyoptabs = np.abs(inlabyopt)
+inyrelativeerror = np.true_divide(inabsoluteerror, inlabyoptabs)
+
+outabsolutesubtract = np.subtract(outlabyopt, outlabyact)
+outabsoluteerror = np.abs(outabsolutesubtract)
+outlabyoptabs = np.abs(outlabyopt)
+outyrelativeerror = np.true_divide(outabsoluteerror, outlabyoptabs)
+
+
+n = scipy.stats.ttest_ind(outlabeff, inlabeff, equal_var=False)
+print "t-statistic is %.10f and the p-value is %.75f." % n
+
+
+n = scipy.stats.ttest_ind(outlabtimes, inlabtimes, equal_var=False)
+print "t-statistic is %.10f and the p-value is %.300f." % n
+
+
+n = scipy.stats.ttest_ind(outlabspeeds, inlabspeeds, equal_var=False)
+print "t-statistic is %.10f and the p-value is %.10f." % n
+
+
+n = scipy.stats.ttest_ind(inovershoot, outovershoot, equal_var=False)
+print "t-statistic is %.10f and the p-value is %.300f." % n
+
+
+n = scipy.stats.ttest_ind(inyrelativeerror, outyrelativeerror, equal_var=False)
+print "t-statistic is %.10f and the p-value is %.25f." % n
+
+#print inhovertime
+#print outhovertime
+
 varianceineff = np.var(inlabeff)
 varianceouteff = np.var(outlabeff)
 varianceintime = np.var(inlabtimes)
@@ -75,7 +222,6 @@ print "in lab time var " + str(varianceintime)
 print "out lab time var " + str(varianceouttime)
 print "in lab speed var " + str(varianceinspeed)
 print "out lab speed var " + str(varianceoutspeed)
-'''
 
 print np.median(inlabeff)
 print np.median(inlabspeeds)
@@ -84,8 +230,6 @@ print np.median(outlabeff)
 print np.median(outlabspeeds)
 print np.median(outlabtimes)
 
-
-'''
 efftest = scipy.stats.ttest_ind(inlabeff, outlabeff, equal_var=True)
 timetest = scipy.stats.ttest_ind(outlabtimes, inlabtimes, equal_var=True)
 speedtest = scipy.stats.ttest_ind(inlabspeeds, outlabspeeds, equal_var=False)
@@ -108,3 +252,5 @@ print scipy.stats.ttest_ind(varianceoutspeed, varianceinspeed, equal_var=False)
 #print "In variance: " + np.var(inlabeff)
 #print "Out variance: " + np.var(outlabeff)
 '''
+
+print len(inlabeff) + len(outlabeff) -1
