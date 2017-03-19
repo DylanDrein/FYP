4cece99cfc7dcb39b7d1fa20cf273b50d79b9cbe
DylanDrein <dylandrein@gmail.com>
2017-03-19 02:37:02 +0000

    errthing

--- EfficiencyWithTimeGraphs.py
+++ EfficiencyWithTimeGraphs.py
@@ -38,21 +38,38 @@ with handle4 as infile:
 
 bins = np.arange(0, 1.01, 0.01)
 
-print len(inlabeff)
-print len(outlabeff)
-print len(inlabtimes)
-print len(outlabtimes)
+#print len(inlabeff)
+#print len(outlabeff)
+#print len(inlabtimes)
+#print len(outlabtimes)
 
 #inlabeff = np.log2(inlabeff)
 #outlabeff = np.log2(outlabeff)
 
-print inlabeff
-print outlabeff
-
+#print inlabeff
+#print outlabeff
+'''
+inlabeff = np.ma.masked_equal(inlabeff,0)
+outlabeff = np.ma.masked_equal(outlabeff,0)
 
 sdvinlab = np.std(inlabeff)
 meaninlab = np.mean(inlabeff)
 varinlab = np.var(inlabeff)
+sdvoutlab = np.std(outlabeff)
+meanoutlab = np.mean(outlabeff)
+varoutlab = np.var(outlabeff)
+'''
+
+inlabtimes = np.ma.masked_equal(inlabtimes,0)
+outlabtimes = np.ma.masked_equal(outlabtimes,0)
+
+sdvinlab = np.std(inlabtimes)
+meaninlab = np.mean(inlabtimes)
+varinlab = np.var(inlabtimes)
+sdvoutlab = np.std(outlabtimes)
+meanoutlab = np.mean(outlabtimes)
+varoutlab = np.var(outlabtimes)
+'''
 label1 = ["$\overline{x}$ = " + str(meaninlab) + "\n" + "$s$ = " + str(sdvinlab) + "\n" + "$s^2$ = " + str(varinlab)]
 plt.hist(inlabeff, bins, label = label1)
 #plt.hist(inlabeff, bins, label = label1)
@@ -68,9 +85,6 @@ plt.savefig('./FinalGraphs/InLabEfficiencyHist.png')
 #plt.savefig('./Metricgraphs/' + "WeightedinLabHist" + '.png')
 plt.clf()
 
-sdvoutlab = np.std(outlabeff)
-meanoutlab = np.mean(outlabeff)
-varoutlab = np.var(outlabeff)
 label2 = ["$\overline{x}$ = " + str(meanoutlab) + "\n" + "$s$ = " + str(sdvoutlab) + "\n" + "$s^2$ = " + str(varoutlab)]
 plt.hist(outlabeff, bins, label = label2)
 #plt.hist(outlabeff, bins, label = label2)
@@ -84,14 +98,19 @@ plt.legend(loc="best")
 plt.savefig('./FinalGraphs/OutLabEfficiencyHist.png')
 #plt.savefig('./Metricgraphs/' + "WeightedOutOfLabHist" + '.png')
 plt.clf()
-
-print sdvinlab
+'''
 print meaninlab
+print sdvinlab
 print varinlab
-print sdvoutlab
 print meanoutlab
+print sdvoutlab
 print varoutlab
+print min(inlabtimes)
+print max(inlabtimes)
+print min(outlabtimes)
+print max(outlabtimes)
 
+'''
 data = [inlabeff, outlabeff]
 plt.boxplot(data, 0, '', labels=["In lab", "Out of lab"])
 plt.title("Efficiency boxplot")
@@ -99,3 +118,4 @@ plt.ylabel('Efficiency (optimal/actual path lengths)')
 #plt.savefig('./FinalGraphs/TotalEfficiencyBoxPlotNoNotchesNoOutliers.png')
 plt.show()
 plt.clf()
+'''
